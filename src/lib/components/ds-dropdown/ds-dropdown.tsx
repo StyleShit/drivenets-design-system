32bb99f3981e256fa5118964d0c838b7911ca261
aroba-dn <aroba@drivenets.com>
2025-07-09 18:09:54 +0300

    AR-3643: Main menu navigation

--- src/lib/components/ds-dropdown/ds-dropdown.tsx
+++ src/lib/components/ds-dropdown/ds-dropdown.tsx
@@ -9,68 +9,67 @@ import { DsDropdownProps } from './ds-dropdown.types';
  * Design system Dropdown component
  */
 const DsDropdown: React.FC<DsDropdownProps> = ({
-  options,
-  selectedHref,
-  onSelect,
-  children,
-  contentGap = 0,
+	options,
+	selectedHref,
+	onSelect,
+	children,
+	contentGap = 0,
 }) => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [selectedItem, setSelectedItem] = useState('');
-  const [open, setOpen] = useState(false);
+	const [searchTerm, setSearchTerm] = useState('');
+	const [selectedItem, setSelectedItem] = useState('');
+	const [open, setOpen] = useState(false);
 
-  useEffect(() => {
-    if (selectedHref !== undefined) {
-      setSelectedItem(selectedHref);
-    }
-  }, [selectedHref]);
+	useEffect(() => {
+		if (selectedHref !== undefined) {
+			setSelectedItem(selectedHref);
+		}
+	}, [selectedHref]);
 
-  const filteredOptions = options.filter(option =>
-    option.label.toLowerCase().includes(searchTerm.toLowerCase()),
-  );
+	const filteredOptions = options.filter((option) =>
+		option.label.toLowerCase().includes(searchTerm.toLowerCase()),
+	);
 
-  return (
-    <DropdownMenu.Root open={open} onOpenChange={setOpen}>
-      <DropdownMenu.Trigger asChild>{children}</DropdownMenu.Trigger>
-      <DropdownMenu.Content
-        className={classNames(styles.content, styles.viewport)}
-        sideOffset={contentGap}
-      >
-        <div className={styles.searchInput}>
-          <input
-            type="text"
-            className={styles.searchInputField}
-            placeholder="Search"
-            value={searchTerm}
-            onChange={e => setSearchTerm(e.target.value)}
-          />
-          <DsIcon className={styles.searchIcon} icon="search" />
-        </div>
-        {filteredOptions.map((option, idx) => {
-          const isSelected = selectedItem === option.href;
-          return (
-            <DropdownMenu.Item key={idx} asChild>
-              <a
-                href={option.href}
-                className={classNames(styles.item, {
-                  [styles.selected]: isSelected,
-                })}
-                onClick={e => {
-                  e.preventDefault();
-                  setSelectedItem(option.href);
-                  onSelect?.(option.href);
-                  setOpen(false);
-                }}
-              >
-                <span className={styles.indicator}>{isSelected && <DsIcon icon="check" />}</span>
-                {option.label}
-              </a>
-            </DropdownMenu.Item>
-          );
-        })}
-      </DropdownMenu.Content>
-    </DropdownMenu.Root>
-  );
+	return (
+		<DropdownMenu.Root open={open} onOpenChange={setOpen}>
+			<DropdownMenu.Trigger asChild>{children}</DropdownMenu.Trigger>
+			<DropdownMenu.Content className={classNames(styles.content, styles.viewport)} sideOffset={contentGap}>
+				<div className={styles.searchInput}>
+					<DsIcon className={styles.searchIcon} icon="search" size="tiny" />
+					<input
+						type="text"
+						className={styles.searchInputField}
+						placeholder="Search"
+						value={searchTerm}
+						onChange={(e) => setSearchTerm(e.target.value)}
+					/>
+				</div>
+				<div className={styles.items}>
+					{filteredOptions.map((option, idx) => {
+						const isSelected = selectedItem === option.href;
+						return (
+							<DropdownMenu.Item key={idx} asChild>
+								<a
+									href={option.href}
+									className={classNames(styles.item, {
+										[styles.selected]: isSelected,
+									})}
+									onClick={(e) => {
+										e.preventDefault();
+										setSelectedItem(option.href);
+										onSelect?.(option.href);
+										setOpen(false);
+									}}
+								>
+									<span className={styles.indicator}>{isSelected && <DsIcon icon="check" />}</span>
+									{option.label}
+								</a>
+							</DropdownMenu.Item>
+						);
+					})}
+				</div>
+			</DropdownMenu.Content>
+		</DropdownMenu.Root>
+	);
 };
 
 export default DsDropdown;
