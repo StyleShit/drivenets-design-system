a84157c73e3b4bc7d1d17125571ff034d5142336
StyleShit <32631382+StyleShit@users.noreply.github.com>
2026-02-03 11:08:25 +0200

    wip

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -4,5 +4,5 @@
 export default {
 	'*': 'cspell --no-must-find-files',
 	'!(*.js|*.mjs|*.ts|*.tsx)': 'oxfmt --no-error-on-unmatched-pattern',
-	'*.{js,mjs,ts,tsx}': ['eslint --max-warnings=0', 'oxfmt'],
+	'*.{js,mjs,ts,tsx}': ['oxfmt', 'eslint --max-warnings=0'],
 };
