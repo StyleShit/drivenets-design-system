cdacc4f59c80af1c6caeab70c5ee09ee655a3197
StyleShit <32631382+StyleShit@users.noreply.github.com>
2026-08-16 10:27:22 +0300

    wip

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,11 +1,11 @@
 import { RuleConfigSeverity, type UserConfig } from '@commitlint/types';
-import designSystem from '@drivenets/commitlint-plugin-internal';
+import internal from '@drivenets/commitlint-plugin-internal';
 
 const hasBreakingChanges = process.env.HAS_BREAKING_CHANGES === 'true';
 
 const config: UserConfig = {
 	extends: ['@commitlint/config-conventional'],
-	plugins: [designSystem],
+	plugins: [internal],
 	rules: {
 		'internal/exclamation-mark': [RuleConfigSeverity.Error, hasBreakingChanges ? 'always' : 'never'],
 		'internal/require-jira-ticket': [RuleConfigSeverity.Error, 'always', { prefix: 'AR' }],
